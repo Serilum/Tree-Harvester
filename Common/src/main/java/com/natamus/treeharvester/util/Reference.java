826393492d92c4b0b9d96cf799edf2e0ce045308
Rick South <rick@natamus.com>
2023-12-18 09:57:23 +0100

    (v8.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/treeharvester/util/Reference.java
+++ Common/src/main/java/com/natamus/treeharvester/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.treeharvester.util;
 public class Reference {
 	public static final String MOD_ID = "treeharvester";
 	public static final String NAME = "Tree Harvester";
-	public static final String VERSION = "8.3";
+	public static final String VERSION = "8.4";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
