0df7295164ef3ca2a1b4d897f385b7ce0b03994a
Rick South <rick@natamus.com>
2024-10-26 14:57:10 +0200

    Tree Harvester source code for 1.21.3.

---
+++ NeoForge/src/main/java/com/natamus/treeharvester/neoforge/events/NeoForgeTreeCutEvents.java
@@ -0,0 +1,36 @@
+package com.natamus.treeharvester.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.treeharvester.events.TreeCutEvents;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeTreeCutEvents {
+	@SubscribeEvent
+	public static void onTreeHarvest(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+		
+		TreeCutEvents.onTreeHarvest(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+
+	@SubscribeEvent
+	public static void onHarvestBreakSpeed(PlayerEvent.BreakSpeed e) {
+		Player player = e.getEntity();
+		Level level = player.level();
+
+		float originalSpeed = e.getOriginalSpeed();
+		float newSpeed = TreeCutEvents.onHarvestBreakSpeed(level, player, originalSpeed, e.getState());
+
+		if (originalSpeed != newSpeed) {
+			e.setNewSpeed(newSpeed);
+		}
+	}
+}
