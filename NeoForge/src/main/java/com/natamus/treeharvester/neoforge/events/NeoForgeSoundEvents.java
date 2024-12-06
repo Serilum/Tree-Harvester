08dc788fc78c26a330e48caa4ac05dc2d07e1868
Rick South <rick@natamus.com>
2024-12-06 13:09:36 +0100

    Tree Harvester source code for 1.21.4.

---
+++ NeoForge/src/main/java/com/natamus/treeharvester/neoforge/events/NeoForgeSoundEvents.java
@@ -0,0 +1,17 @@
+package com.natamus.treeharvester.neoforge.events;
+
+import com.natamus.treeharvester.events.SoundEvents;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.sound.PlaySoundEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(value = Dist.CLIENT)
+public class NeoForgeSoundEvents {
+	@SubscribeEvent
+	public static void onSoundEvent(PlaySoundEvent e) {
+		if (!SoundEvents.onSoundEvent(e.getEngine(), e.getOriginalSound())) {
+			e.setSound(null);
+		}
+	}
+}
