0a0a2b4c444f0a147d6fd2f561f0f7bad2c72484
Rick South <rick@natamus.com>
2025-02-12 23:09:43 +0100

    (v9.1) Improved mod performance. [+5]

--- Forge/src/main/java/com/natamus/treeharvester/forge/events/ForgeSoundEvents.java
+++ Forge/src/main/java/com/natamus/treeharvester/forge/events/ForgeSoundEvents.java
@@ -4,12 +4,10 @@ import com.natamus.treeharvester.events.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber(value = Dist.CLIENT)
 public class ForgeSoundEvents {
 	@SubscribeEvent
-	public void onSoundEvent(PlaySoundEvent e) {
+	public static void onSoundEvent(PlaySoundEvent e) {
 		if (!SoundEvents.onSoundEvent(e.getEngine(), e.getOriginalSound())) {
 			e.setSound(null);
 		}
