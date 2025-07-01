6559071baec3e031ff5f3db39da975930535f714
Rick South <rick@natamus.com>
2025-07-01 09:50:57 +0200

    Tree Harvester source code for 1.21.7.

---
+++ Forge/src/main/java/com/natamus/treeharvester/forge/events/ForgeTreeCutEvents.java
@@ -0,0 +1,41 @@
+package com.natamus.treeharvester.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.treeharvester.events.TreeCutEvents;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeTreeCutEvents {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeTreeCutEvents.class);
+	}
+
+	@SubscribeEvent
+	public static void onTreeHarvest(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+		
+		TreeCutEvents.onTreeHarvest(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+
+	@SubscribeEvent
+	public static void onHarvestBreakSpeed(PlayerEvent.BreakSpeed e) {
+		Player player = e.getEntity();
+		Level level = player.level();
+
+		float originalSpeed = e.getOriginalSpeed();
+		float newSpeed = TreeCutEvents.onHarvestBreakSpeed(level, player, originalSpeed, e.getState());
+
+		if (originalSpeed != newSpeed) {
+			e.setNewSpeed(newSpeed);
+		}
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/treeharvester/neoforge/events/NeoForgeSoundEvents.java
@@ -0,0 +1,15 @@
+package com.natamus.treeharvester.neoforge.events;
+
+import com.natamus.treeharvester.events.SoundEvents;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.sound.PlaySoundEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeSoundEvents {
+	@SubscribeEvent
+	public static void onSoundEvent(PlaySoundEvent e) {
+		if (!SoundEvents.onSoundEvent(e.getEngine(), e.getOriginalSound())) {
+			e.setSound(null);
+		}
+	}
+}
