ee4e1058350d67a8bc821b919561713960b92e50
Rick South <rick@natamus.com>
2025-10-03 13:46:21 +0200

    Tree Harvester source code for 1.21.9.

---
+++ Forge/src/main/java/com/natamus/treeharvester/forge/events/ForgeSoundEvents.java
@@ -0,0 +1,24 @@
+package com.natamus.treeharvester.forge.events;
+
+import com.natamus.treeharvester.events.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeSoundEvents {
+	public static void registerEventsInBus() {
+		// BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeSoundEvents.class);
+
+		PlaySoundEvent.BUS.addListener(ForgeSoundEvents::onSoundEvent);
+	}
+
+	@SubscribeEvent
+	public static void onSoundEvent(PlaySoundEvent e) {
+		if (!SoundEvents.onSoundEvent(e.getEngine(), e.getOriginalSound())) {
+			e.setSound(null);
+		}
+	}
+}
