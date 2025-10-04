915ac0285b628de8021d165c1b086712fb4e6a4d
Rick South <rick@natamus.com>
2025-10-04 18:46:01 +0200

    (v9.3) (1.21.9 Forge) Fixed incorrect remnant files included in the mod jar.

--- Common/src/main/java/com/natamus/treeharvester/util/Reference.java
+++ Common/src/main/java/com/natamus/treeharvester/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.treeharvester.util;
 public class Reference {
 	public static final String MOD_ID = "treeharvester";
 	public static final String NAME = "Tree Harvester";
-	public static final String VERSION = "9.2";
+	public static final String VERSION = "9.3";
 	public static final String ACCEPTED_VERSIONS = "[1.21.9]";
 }
