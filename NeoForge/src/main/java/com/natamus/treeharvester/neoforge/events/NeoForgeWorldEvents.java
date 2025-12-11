12d666427e4dcddef6762f4461e5672f04852617
Rick South <rick@natamus.com>
2025-12-11 02:28:11 +0100

    Tree Harvester source code for 1.21.11.

---
+++ NeoForge/src/main/java/com/natamus/treeharvester/neoforge/events/NeoForgeWorldEvents.java
@@ -0,0 +1,20 @@
+package com.natamus.treeharvester.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.treeharvester.events.WorldEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeWorldEvents {
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		WorldEvents.onWorldLoad((ServerLevel)level);
+	}
+}
