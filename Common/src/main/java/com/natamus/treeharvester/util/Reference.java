2f7f4f61f8c031a8b009840658b13d33367af473
Rick South <rick@natamus.com>
2026-05-01 23:47:06 +0200

    (v9.4) (26.1.2 NeoForge) Updated to work with NeoForge version 26.1.2.21-beta and above.

--- Common/src/main/java/com/natamus/treeharvester/util/Reference.java
+++ Common/src/main/java/com/natamus/treeharvester/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.treeharvester.util;
 public class Reference {
 	public static final String MOD_ID = "treeharvester";
 	public static final String NAME = "Tree Harvester";
-	public static final String VERSION = "9.3";
+	public static final String VERSION = "9.4";
 	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
 }
--- NeoForge/src/main/java/com/natamus/treeharvester/neoforge/events/NeoForgeTreeCutEvents.java
+++ NeoForge/src/main/java/com/natamus/treeharvester/neoforge/events/NeoForgeTreeCutEvents.java
@@ -4,13 +4,13 @@ import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.treeharvester.events.TreeCutEvents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 
 public class NeoForgeTreeCutEvents {
 	@SubscribeEvent
-	public static void onTreeHarvest(BlockEvent.BreakEvent e) {
+	public static void onTreeHarvest(BreakBlockEvent e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
